7b94f9edd6d0c88c2987375cd7b3c37ac6241b2e
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-03-21 17:03:00 +0530

    add product folder and add pages.jsx file

---
+++ app/addProduct/page.jsx
@@ -0,0 +1,9 @@
+export default function AddProduct(){
+    return (
+        <>
+            <div className="flex justify-between items-center">
+                <h1 className="font-bold py-10 text-2xl">Add New Product</h1>
+            </div>
+        </>
+    );
+}
--- components/ProductsList.jsx
+++ components/ProductsList.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default async function ProductsList() {
     return (
         <>
@@ -5,6 +7,12 @@ export default async function ProductsList() {
                 <div className="flex justify-between items-center">
                     <h1 className="font-bold py-10 text-2xl">unwir dev assessment </h1>
                 </div>
+            </div>
+
+            <div className="text-right">
+                <Link className="btn btn-primary" href={"/addProduct"}>
+                    Add Product
+                </Link>
             </div>
         </>
     );
